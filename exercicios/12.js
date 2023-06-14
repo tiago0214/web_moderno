0871f099c062042ab0e69fb00c1cb34089ad6e0a
Tiago Souza <tiago0214@gmail.com>
2023-06-14 16:48:30 -0300

    tas

--- exercicios/12.js
+++ exercicios/12.js
@@ -1,15 +1,1 @@
-class Pessoa {
-    nome;
-    idade;
-
-    constructor(nome, idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
-
-    descrever() {
-        console.log(`Nome: ${this.nome}; Idade: ${this.idade};`);
-    }
-}
-
-new Pessoa('Renan', 30).descrever()
+console.log('\u10dA')
